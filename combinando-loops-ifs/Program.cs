8db7ae44c74d13836d2971bc93b9747aaacd796d
Leandro <leandro.feitoza@gmail.com>
2021-09-22 21:13:35 -0300

    Exercicio simples praticar sintaxe do loop no C#

---
+++ combinando-loops-ifs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/*
+    Programa encontra a soma de todos os inteiros de 1 a 20
+    divisiveis por 3.
+*/
+
+for (int i = 1; i <= 20; i ++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} é divisível por 3");
+    }
+}
